ab930cc7d5823a8e719f08f316f3a37ea2a9ff15
MAJIN123 <806379655@qq.com>
2016-12-04 21:18:23 +0800

    补充

--- BinaryTree/main.cpp
+++ BinaryTree/main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include<fstream>
+#include <stack>
 using namespace std;
 template <class T>
 struct BinTreeNode{
@@ -22,6 +24,9 @@ protected:
     void InOrder(BinTreeNode<T> *root);              //中序便利
     void PostOrder(BinTreeNode<T> *root);           //后序便利
     void Travers(BinTreeNode<T>* subTree,ostream& out); //前序遍历输出
+    void Insert(BinTreeNode<T> *&root,T x);//插入
+    void Norecursive_PreOrder(BinTreeNode<T>* root);//非递归前序遍历
+   // void CreateBinaryTree(fstream& in,BinTreeNode<T>* &subTree);//建立二叉树
     friend ostream& operator<<(ostream& out,BinaryTree<T> &Tree){
         //重载输出
         out<<"二叉树前序遍历输出：\n";
@@ -38,8 +43,15 @@ public:
     int Height(){return Height(root);}
     int Size(){return Size(root);}
     void PreOrder(){PreOrder(root);}
+    void Norecursive_PreOrder(){Norecursive_PreOrder(root);}//非递归前序遍历
     void InOrder(){InOrder(root);}
     void PostOrder(){PostOrder(root);}
+    void Insert(T x){Insert(root,x);}
+    /*
+    void CreateBinaryTree(fstream& in){
+        CreateBinaryTree(in,root);
+    }//建立二叉树
+     */
     BinTreeNode<T>* Parent(BinTreeNode<T> *current){
         return(root==NULL||root==current)?NULL:Parent(root,current);
     }
@@ -50,9 +62,66 @@ public:
         return (current!=NULL)?current->rightChild:NULL;
     }
     BinTreeNode<T>* getRoot()const{return root;}
-
-
 };
+/*
+template <class T>
+void BinaryTree<T>::CreateBinaryTree(fstream &in, BinTreeNode<T> * &subTree) {
+    T item;
+    if(!in.eof()){
+        in>>item;
+        if(item!=RefValue){
+            subTree=new BinTreeNode<T>(item);
+            CreateBinaryTree(in,subTree->leftChild);
+            CreateBinaryTree(in,subTree->rightChild);
+        }
+        else subTree=NULL;
+
+    }
+}
+ */
+template <class T>
+void BinaryTree<T>::Norecursive_PreOrder(BinTreeNode<T> *root) {
+    /*
+    stack<BinTreeNode<T> *>s;       //第一种方法
+    s.push(NULL);
+    BinTreeNode<T>* p=root;
+    while(p){
+        cout<<p->data<<" ";
+        if(p->rightChild)s.push(p->rightChild);
+        if(p->leftChild)p=p->leftChild;
+        else {
+            p=s.top();
+            s.pop();
+        }
+    }
+    cout<<endl;
+    */
+    stack<BinTreeNode<T> *> S;
+    BinTreeNode<T> *p;
+    S.push(root);
+    while(S){
+        p=S.top();
+        S.pop();
+        cout<<p->data<<" ";
+        if(p->rightChild)S.push(p->rightChild);
+        if(p->leftChild)S.push(p->leftChild);
+    }
+}
+template<class T>
+void BinaryTree<T>::Insert(BinTreeNode<T> *&root, T x) {
+    if(root==NULL){
+        BinTreeNode<T> *p=new BinTreeNode<T>(x);
+        root=p;
+    }
+    else{
+        if(Height(root->leftChild)>Height(root->rightChild)){
+            Insert(root->rightChild,x);
+        }
+        else{
+            Insert(root->leftChild,x);
+        }
+    }
+}
 template <class T>
 void BinaryTree<T>::Travers(BinTreeNode<T> *subTree, ostream &out) {
     if(subTree){
@@ -103,7 +172,7 @@ int BinaryTree<T>::Size(BinTreeNode<T> *subTree) {
     }
 }
 template <class T>
-int BinaryTree::Height(BinTreeNode<T> *subTree) {
+int BinaryTree<T>::Height(BinTreeNode<T> *subTree) {
     if(subTree==NULL)
         return 0;
     else {
@@ -138,6 +207,12 @@ void BinaryTree<T>::destroy(BinTreeNode<T>* &subTree) {
 }
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    BinaryTree<int> p;
+    p.Insert(3);
+    p.Insert(4);
+    p.Insert(1);
+    p.PreOrder();
+    p.Norecursive_PreOrder();
+
+
 }
