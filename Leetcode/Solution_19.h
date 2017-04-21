3e32d9e8f6d2ae86f79b392b096a170c9572f3e5
MAJIN123 <806379655@qq.com>
2017-04-21 12:49:43 +0800

    Remove Nth Node From End of List

--- Leetcode/Solution_19.h
+++ Leetcode/Solution_19.h
@@ -10,6 +10,10 @@ using namespace std;
 #endif //LEETCODE_SOLUTION_19_H
 
 /**
+ * Remove Nth Node From End of List
+ * Given linked list: 1->2->3->4->5, and n = 2.
+ * After removing the second node from the end, the linked list becomes 1->2->3->5.
+ * 
  * Definition for singly-linked list.
  */
 struct ListNode {
