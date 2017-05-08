b3bca151de16586b8d50eadd30b615f756a549f7
MAJIN123 <806379655@qq.com>
2017-05-08 21:04:53 +0800

    count and say

--- Leetcode/Solution_38.h
+++ Leetcode/Solution_38.h
@@ -27,58 +27,61 @@ public:
      * @return
      */
     string countAndSay(int n) {
-        if(n==0)
+        if (n == 0)
             return "";
-        string res="1";
-        while(--n){
-            string cur="";
-            for(int i=0;i<res.size();++i){
-                int count=1;
-                while(i+1<res.size()&&res[i]==res[i+1]){
-                    count++;i++;
+        string res = "1";
+        while (--n) {
+            string cur = "";
+            for (int i = 0; i < res.size(); ++i) {
+                int count = 1;
+                while (i + 1 < res.size() && res[i] == res[i + 1]) {
+                    count++;
+                    i++;
                 }
-                cur+=to_string(count)+res[i];
+                cur += to_string(count) + res[i];
             }
-            res=cur;
+            res = cur;
         }
         return res;
     }
-    int Strtoint(string a){
-        int rel=0;
-        for(int i=0;i<a.size();++i){
-            int tmp=a[i]-'0';
-            rel=rel*10+tmp;
+
+    int Strtoint(string a) {
+        int rel = 0;
+        for (int i = 0; i < a.size(); ++i) {
+            int tmp = a[i] - '0';
+            rel = rel * 10 + tmp;
         }
         return rel;
     }
+
     /**
      * 21 is read off as "one 2, then one 1" or 1211.
      * @param n
      * @return
      */
-    string js(int n){
-        string rel="";
+    string js(int n) {
+        string rel = "";
         vector<int> p;
-        while(n>0){
-            int a=n%10;
-            p.push_back(n%10);//取最后
-            n=n/10;
+        while (n > 0) {
+            int a = n % 10;
+            p.push_back(n % 10);//取最后
+            n = n / 10;
         }
-        int num=*(p.end()-1),count=0;
-        while(p.size()>0){
-            int tmp=*(p.end()-1);
+        int num = *(p.end() - 1), count = 0;
+        while (p.size() > 0) {
+            int tmp = *(p.end() - 1);
             p.pop_back();
-            if(tmp==num)
+            if (tmp == num)
                 count++;
-            else{
-                rel+=char(count+'0');
-                rel+=char(num+'0');
-                num=tmp;
-                count=1;
+            else {
+                rel += char(count + '0');
+                rel += char(num + '0');
+                num = tmp;
+                count = 1;
             }
         }
-        rel+=char(count+'0');
-        rel+=char(num+'0');
+        rel += char(count + '0');
+        rel += char(num + '0');
         return rel;
     }
 };
