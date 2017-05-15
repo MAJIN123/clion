795f5fcf183ea59148d970aa324e1d2005e38374
MAJIN123 <806379655@qq.com>
2017-05-15 16:50:01 +0800

    kmp once more

--- Leetcode/add_KMP.h
+++ Leetcode/add_KMP.h
@@ -10,41 +10,40 @@ using namespace std;
 
 #endif //LEETCODE_ADD_KMP_H
 
-class Solution_kmp{
+class Solution_kmp {
 public:
-    vector<int> get_next(string s){
-        int len=s.length();
+    vector<int> get_next(string s) {
+        int len = s.length();
         vector<int> next(len);
-        next[0]=-1;
-        int j=-1;
-        int i=0;
-        while(i<len){
-            if(j==-1||s[j]==s[i])    {
+        next[0] = -1;
+        int j = -1;
+        int i = 0;
+        while (i < len) {
+            if (j == -1 || s[j] == s[i]) {
                 ++i;
                 ++j;
-                next[i]=j;
-            }
-            else
-                j=next[j];
+                next[i] = j;
+            } else
+                j = next[j];
         }
         return next;
     }
-    int KMP(string s,string p){
-        vector<int> next=get_next(p);
-        int i=0;
-        int j=0;
-        int slen=s.length();
-        int plen=p.length();
-        while(i<slen&&j<plen){
-            if(j==-1||s[i]==p[j]){
+
+    int KMP(string s, string p) {
+        vector<int> next = get_next(p);
+        int i = 0;
+        int j = 0;
+        int slen = s.length();
+        int plen = p.length();
+        while (i < slen && j < plen) {
+            if (j == -1 || s[i] == p[j]) {
                 ++i;
                 ++j;
-            }
-            else
-                j=next[j];
+            } else
+                j = next[j];
         }
-        if(j==plen)
-            return i-j;
+        if (j == plen)
+            return i - j;
         else return -1;
     }
 };
