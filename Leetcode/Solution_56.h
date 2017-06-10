913be3ac0fbcfae8bd7353753ea6a509f5400034
MAJIN123 <806379655@qq.com>
2017-06-10 18:53:37 +0800

    56,57,58

--- Leetcode/Solution_56.h
+++ Leetcode/Solution_56.h
@@ -104,6 +104,16 @@ public:
         return intervals;
     }
 };
+/**
+ * Length of Last Word
+ * Given a string s consists of upper/lower-case alphabets and empty space characters ' ',
+ * return the length of last word in the string.
+ * If the last word does not exist, return 0.
+ * Note: A word is defined as a character sequence consists of non-space characters only.
+ * For example,
+ * Given s = "Hello World",
+ * return 5.
+ */
 class Solution_58 {
 public:
     int lengthOfLastWord(string s) {
