49e04d722d3bf9f51a6d19871d45f0fd915fb54a
MAJIN123 <806379655@qq.com>
2017-06-10 20:03:31 +0800

    54,59

--- Leetcode/Solution_54.h
+++ Leetcode/Solution_54.h
@@ -51,6 +51,18 @@ private:
         hh(matrix, a, b, x, y, res);
     }
 };
+/**
+ * Spiral Matrix II
+ * Given an integer n, generate a square matrix filled with elements from 1 to n2 in spiral order.
+ * For example,
+ * Given n = 3,
+ * You should return the following matrix:
+    [
+     [ 1, 2, 3 ],
+     [ 8, 9, 4 ],
+     [ 7, 6, 5 ]
+    ]
+ */
 class Solution_54_b {
 public:
     vector<int> spiralOrder(vector<vector<int>> &matrix) {
@@ -73,5 +85,60 @@ public:
             if (++l > r) break;
         }
         return spiral;
+    }
+};
+class Solution_59 {
+public:
+    vector<vector<int>> generateMatrix(int n) {
+        int dig = 1;
+        vector<vector<int>> res(n, vector<int>(n, 0));
+        int u = 0, d = n - 1, l = 0, r = n - 1;
+        while (dig <= n * n) {
+            for (int col = l; col <= r; ++col) {
+                res[u][col] = dig;
+                ++dig;
+            }
+            ++u;
+            for (int row = u; row <= d; ++row) {
+                res[row][r] = dig;
+                ++dig;
+            }
+            --r;
+            for (int col = r; col >= l; --col) {
+                res[d][col] = dig;
+                ++dig;
+            }
+            --d;
+            for (int row = d; row >= u; --row) {
+                res[row][l] = dig;
+                ++dig;
+            }
+            ++l;
+        }
+        return res;
+    }
+};
+class Solution_59_b {
+public:
+    vector<vector<int> > generateMatrix(int n) {
+        vector<vector<int> > ret(n, vector<int>(n));
+        int k = 1, i = 0;
+        while (k <= n * n) {
+            int j = i;
+            // four steps
+            while (j < n - i)             // 1. horizonal, left to right
+                ret[i][j++] = k++;
+            j = i + 1;
+            while (j < n - i)             // 2. vertical, top to bottom
+                ret[j++][n - i - 1] = k++;
+            j = n - i - 2;
+            while (j > i)                  // 3. horizonal, right to left
+                ret[n - i - 1][j--] = k++;
+            j = n - i - 1;
+            while (j > i)                  // 4. vertical, bottom to  top
+                ret[j--][i] = k++;
+            i++;      // next loop
+        }
+        return ret;
     }
 };
